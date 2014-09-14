16de0b88eb69f079692b142c73fc6b6df9213eca
KUBO, Koichi <k@obuk.org>
2014-09-14 11:17:33 +0900

    fixes utf-8 strings in verbatim paragraphs,
    and reduces the damage in the error of perltidy that may be caused by
    utf-8. the damage makes dizzy paragraphs.

--- lib/Perldoc/Server/Controller/Ajax/PerlSyntax.pm
+++ lib/Perldoc/Server/Controller/Ajax/PerlSyntax.pm
@@ -9,6 +9,10 @@ use HTML::Entities;
 use OpenThought;
 use Perl::Tidy;
 
+use HTML::Entities;
+use CGI::Util qw(unescape);
+use Encode;
+
 =head1 NAME
 
 Perldoc::Server::Controller::Ajax::PerlSyntax - Catalyst Controller
@@ -34,6 +38,16 @@ sub index :Path :Args(0) {
   
   $code = decode_entities($code);
 
+  # fixes utf-8 strings in verbatim paragraphs, and reduces the damage
+  # in the error of perltidy that may be caused by utf-8. the damage
+  # makes dizzy paragraphs.
+
+  # the utf-8 escaped strings that passed from OpenThought.js. but
+  # puts the fixes here with perltidy experimentally.  because I don't
+  # know how the OpenThought.js works.
+
+  $code =~ s/%u[\da-fA-F]{4,}/decode('utf8', unescape($&))/ge;
+
   my ($result,$error);
   perltidy(
     source      => \$code,
@@ -43,6 +57,38 @@ sub index :Path :Args(0) {
     stderr      => File::Spec->devnull(),
   );
   
+  # run perltidy for each line to reduce the damage if error.
+  if ($error) {
+      my @result;
+      for (split("\n", $code)) {
+	  ($result, $error) = ();
+	  perltidy(
+	      source      => \$_,
+	      destination => \$result,
+	      argv        => ['-html','-pre'],
+	      errorfile   => \$error,
+	      stderr      => File::Spec->devnull(),
+	      );
+
+	  $result =~ s!\n*</?pre>\n*!!g;
+
+	  # the tidy style "q" (quote) is same as error, so remove it
+	  # and adds style "w" (bareword) for \w+ to make link. tidy
+	  # style is defined in %Perl::Tidy::short_to_long_names.
+	  if ($result =~ s!^<span class="q">(.*)</span>$!$1! ||
+	      $result !~ /<span\b/) {
+	      $result =~ s!\w+!<span class="w">$&</span>!g;
+	  }
+	  push(@result, $result);
+      }
+      $result = join("\n", "<pre>", @result, "</pre>");
+  }
+
+  # set $show_perltidy = 1 to show the result of perltidy after the
+  # verbatim paragraph.
+  my $raw_result; my $show_perltidy = 0;
+  $raw_result = encode_entities($result) if $show_perltidy;
+
   $result =~ s!\$!&#36;!g;
   $result =~ s!\n*</?pre.*?>\n*!!g;
   $result =~ s!<span class="k">(.*?)</span>!($c->model('PerlFunc')->exists($1))?q(<a class="l_k" href=").qq(/functions/$1">$1</a>):$1!sge;
@@ -52,6 +98,7 @@ sub index :Path :Args(0) {
   my @lines = split(/\r\n|\n/,$result);
   foreach (@lines) {$output .= "<li>$_</li>"}
   $output .= '</ol>';
+  $output .= "<pre>$raw_result</pre>" if $show_perltidy;
 
   push @{$c->stash->{openthought}}, {$id => $output};
   $c->detach('View::OpenThoughtTT');
