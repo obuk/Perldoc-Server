a86f3418a90389c98604117c0dd5b41fe5c93bf9
KUBO, Koichi <k@obuk.org>
2015-06-08 06:39:21 +0900

    adds pdf cache

--- lib/Perldoc/Server/View/Pod2Pdf.pm
+++ lib/Perldoc/Server/View/Pod2Pdf.pm
@@ -4,34 +4,65 @@ use strict;
 use warnings;
 use parent 'Catalyst::View';
 use Pod::Lualatex;
-use Cwd;
-use File::Temp qw/ tempfile tempdir /;
-use Perl6::Slurp;
+use File::Temp qw/ tempdir /;
+use File::Spec::Functions qw/ catfile /;
+use File::Slurp qw/ read_file write_file /;
+use Digest::MD5 qw/ md5_base64 /;
+use Encode;
 
 sub process {
   my ($self, $c) = @_;
 
-  my $pdf;
-  my $dir = tempdir(CLEANUP => 1);
-  # my $dir = 'tmp'; mkdir $dir;
-  my $cwd = getcwd;
-  chdir $dir;
-  open my $in_fh, "<", \$c->stash->{pod};
-  open my $out_fh, ">", "texput.tex";
-  my $parser = Pod::Lualatex->new();
-  $parser->parse_from_filehandle($in_fh, $out_fh);
-  close $in_fh;
-  close $out_fh;
-  open STDIN, '<', '/dev/null';
-  system "lualatex", "texput.tex" for 1..2;
-  eval { $pdf = slurp "texput.pdf" };
-  die "can't convert into pdf; install tex-luatex\n" if $@;
-  unlink glob "texput.*";
-  chdir $cwd;
-  rmdir $dir;
-
   (my $pdf_filename = $c->stash->{title}) =~ s/(::|\/|\s+)/-/g;
   $pdf_filename .= '.pdf' unless ($pdf_filename =~ /\.pdf$/i);
+
+  my ($pdf, $pdf_cache);
+  my ($md5, $md5_cache) = md5_base64($c->stash->{pod});
+
+  if (my $cache = $ENV{PDF_CACHE}) {
+    $cache = undef unless -d $cache;
+    if ($cache) {
+      $pdf_cache = catfile $cache, $pdf_filename;
+      ($md5_cache = $pdf_cache) =~ s/\.pdf$/.md5/;
+    }
+    if (-r $md5_cache && -r $pdf_cache &&
+          $md5 eq read_file($md5_cache)) {
+      $pdf = read_file $pdf_cache;
+    }
+  }
+
+  unless ($pdf) {
+    my $tempdir = tempdir(CLEANUP => 1);
+    # my $tempdir = 'tmp'; mkdir $tempdir unless -d $tempdir;
+    my $texput_tex = catfile $tempdir, 'texput.tex';
+    my $texput_pdf = catfile $tempdir, 'texput.pdf';
+    my $texput_log = catfile $tempdir, 'texput.log';
+    open my $in_fh, "<", \$c->stash->{pod};
+    open my $out_fh, ">", $texput_tex;
+    my $parser = Pod::Lualatex->new();
+    $parser->parse_from_filehandle($in_fh, $out_fh);
+    close $in_fh;
+    close $out_fh;
+    my $cmd = join('; ', "chdir $tempdir", ("cat texput.tex | lualatex") x 2);
+    system "($cmd)";
+    die "can't run lualatex\n" if $@;
+    chop(my @log = read_file($texput_log));
+    if (grep { /Fatal error occurred/ } @log) {
+      @log = map { eval { decode('UTF-8', $_) } // '' } @log;
+      $c->stash->{pod2html} = join("\n", '<pre>', @log, '</pre>');
+      $c->stash->{page_template} = 'pod2html.tt';
+      $c->forward('View::TT');
+      return;
+    }
+    $pdf = read_file $texput_pdf;
+    die "can't read $texput_pdf\n" unless $pdf;
+    if ($pdf_cache) {
+      write_file($pdf_cache, $pdf);
+      write_file($md5_cache, $md5);
+    }
+    unlink glob catfile($tempdir, '*');
+    rmdir $tempdir if -d $tempdir;
+  }
 
   # my $pdf_disposition = 'attachment';
   my $pdf_disposition = 'inline';
